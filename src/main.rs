3a7c05580a37a29d3caca99607fc83333311dda5
Mihail Antonov <antonovma@outlook.com>
2022-06-26 17:58:45 +0400

    works

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,7 @@
-use std::thread;
-use std::time::Duration;
-
-use glib::{clone, Continue, MainContext, PRIORITY_DEFAULT};
 use gtk::prelude::*;
-use gtk::{glib, Application, ApplicationWindow, Button};
+use gtk::{Application, ApplicationWindow, Button};
 
-const APP_ID: &str = "org.gtk_rs.MainEventLoop3";
+const APP_ID: &str = "org.gtk-rs.HelloWorld3";
 
 fn main() {
     let app = Application::builder().application_id(APP_ID).build();    // Create a new application
@@ -14,7 +10,7 @@ fn main() {
 }
 
 fn build_ui(app: &Application) {
-    // Create a button
+    // Create a button with label and margins
     let button = Button::builder()
         .label("Press me!")
         .margin_top(12)
@@ -23,39 +19,19 @@ fn build_ui(app: &Application) {
         .margin_end(12)
         .build();
 
-    let (sender, receiver) = MainContext::channel(PRIORITY_DEFAULT);
     // Connect to "clicked" signal of `button`
-    button.connect_clicked(move |_| {
-        let sender = sender.clone();
-        // The long running operation runs now in a separate thread
-        thread::spawn(move || {
-            // Deactivate the button until the operation is done
-            sender.send(false).expect("Could not send through channel");
-            let ten_seconds = Duration::from_secs(10);
-            thread::sleep(ten_seconds);
-            // Activate the button again
-            sender.send(true).expect("Could not send through channel");
-        });
+    button.connect_clicked(move |button| {
+        // Set the label to "Hello World!" after the button has been clicked on
+        button.set_label("Hello World!");
     });
-
-    // The main loop executes the closure as soon as it receives the message
-    receiver.attach(
-        None,
-        clone!(@weak button => @default-return Continue(false),
-                    move |enable_button| {
-                        button.set_sensitive(enable_button);
-                        Continue(true)
-                    }
-        ),
-    );
 
     // Create a window
     let window = ApplicationWindow::builder()
         .application(app)
-        .title("My GTK App")
+        .title("My GTK App") // Window title
         .child(&button)
         .build();
 
     window.present();       // Present window
-    window.show_all();
+    window.show_all();      // Will not work with no show_all
 }
